82bd2a5316f4eaee34d197d64a666afbc112b754
Gabe <afoste53@gmail.com>
2021-10-28 10:23:21 -0500

    add getByID

--- main.go
+++ main.go
@@ -24,6 +24,8 @@ var albums = []album{
 func main(){
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -45,5 +47,20 @@ func postAlbums(c *gin.Context){
 	// add new album to albums slice
 	albums = append(albums, newAlbum)
 
-	c.IndentedJSON(http:StatusCreated, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID basic getter
+func getAlbumByID(c *gin.Context){
+	id := c.Param("id")
+
+	// loop and find specified id
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "album not found"})
 }
